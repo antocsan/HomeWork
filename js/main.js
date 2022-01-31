782c6ae4d21575792e787085d9522f467d94e51f
antocsan <93600250+antocsan@users.noreply.github.com>
2022-01-31 14:20:34 +0300

    feat:scroll up

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    //modal
     var modal = $('.modal'),
         modalBtn = $('[data-toggle=modal]'),
         closeBtn = $('.modal__close');
@@ -7,7 +8,20 @@ $(document).ready(function() {
         modal.toggleClass('modal--visible');
     })
     closeBtn.on('click', function() {
-        modal.toggleClass('modal--visible');
-    })
+            modal.toggleClass('modal--visible');
+        })
+        //modal
 
+
+    //scroll 
+    $('a[href^="#"').on('click', function() {
+
+        let href = $(this).attr('href');
+
+        $('html, body').animate({
+            scrollTop: $(href).offset().top
+        });
+        return false;
+    });
+    //scroll 
 });
