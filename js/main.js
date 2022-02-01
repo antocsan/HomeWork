e70864bbd47f0168e02ef087f8d2336f4674b6c6
antocsan <93600250+antocsan@users.noreply.github.com>
2022-02-01 21:54:16 +0300

    feat:add project section

--- js/main.js
+++ js/main.js
@@ -24,4 +24,24 @@ $(document).ready(function() {
         return false;
     });
     //scroll 
+    const swiper = new Swiper('.swiper', {
+        loop: true,
+
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets'
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        // And if we need scrollbar
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+    })
+
 });
