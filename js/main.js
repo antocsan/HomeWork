c0fd73b40fa427ed451771e79115786dc95a48b1
antocsan <93600250+antocsan@users.noreply.github.com>
2022-02-11 20:20:49 +0300

    feat:add validation

--- js/main.js
+++ js/main.js
@@ -25,25 +25,70 @@ $(document).ready(function() {
     });
     //scroll 
     const swiper = new Swiper('.swiper', {
-        loop: true,
+            loop: true,
 
-        pagination: {
-            el: '.swiper-pagination',
-            type: 'bullets'
-        },
+            pagination: {
+                el: '.swiper-pagination',
+                type: 'bullets'
+            },
 
-        // Navigation arrows
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
+            // Navigation arrows
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
 
-        // And if we need scrollbar
-        scrollbar: {
-            el: '.swiper-scrollbar',
-        },
-    })
-
+            // And if we need scrollbar
+            scrollbar: {
+                el: '.swiper-scrollbar',
+            },
+        })
+        //wow animate
     new WOW().init();
+
+
+    //validation form
+
+
+    control__form
+    $(".control__form").validate({
+        errorClass: "invalid",
+        rules: {
+            userName: "required",
+            userPhone: "required",
+        },
+        messages: {
+            userName: "Пожалуйста введите ваше имя",
+            userPhone: "Пожалуйста введите ваш телефон",
+
+        }
+    });
+
+
+    $(".modal__form").validate({
+        errorClass: "invalid",
+        rules: {
+            userName: "required",
+            userPhone: "required",
+            userEmail: {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            userName: "Пожалуйста введите ваше имя",
+            userPhone: "Пожалуйста введите ваш телефон",
+            userEmail: {
+                required: "Пожалуйста укажите ваш Email",
+                email: "Укажите Email в формате : name@domain.com"
+            }
+        }
+    });
+
+
+    // mask for form
+
+    $('[type=tel]').mask('+375(00)000-00-00', { placeholder: "+375(__)___-__-__" });
+
 
 });
